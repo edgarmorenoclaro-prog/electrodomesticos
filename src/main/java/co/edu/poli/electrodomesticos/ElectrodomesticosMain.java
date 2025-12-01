c04ea58114af37270ec8f4e90323788b20a34738
Edgar Moreno <edgarmorenoclaro@gmail.com>
2025-12-01 10:11:04 -0500

    proyecto poo electrodomesticos

---
+++ src/main/java/co/edu/poli/electrodomesticos/ElectrodomesticosMain.java
@@ -0,0 +1,12 @@
+package co.edu.poli.electrodomesticos;
+
+/**
+ *
+ * @author SALON206
+ */
+public class ElectrodomesticosMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
